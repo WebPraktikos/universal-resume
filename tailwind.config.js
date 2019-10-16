6aa79c420255c42e3de52e10335008fdb89fe0e1
WebPraktikos <hi@webpraktikos.com>
2019-10-16 02:05:08 +0200

    fix: Calculate new column height

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,10 +42,10 @@ module.exports = {
       },
       height: {
         'letter': '85.9375rem',
-        'letter-col': '72.1375rem',
+        'letter-col': '71.952937rem',
         'letter-col-full': '77.9375rem',
         'a4': '91.350883rem',
-        'a4-col': '77.550883rem',
+        'a4-col': '77.36632rem',
         'a4-col-full': '83.350883rem',
       },
       spacing: {
