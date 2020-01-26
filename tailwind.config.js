c6e435a73f2d9714f84b5dcf48cdab7ca43eaf85
WebPraktikos <hi@webpraktikos.com>
2020-01-26 20:01:50 +0100

    design: Change spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,10 +42,10 @@ module.exports = {
       },
       height: {
         'letter': '85.9375rem',
-        'letter-col': '72.077937rem',
+        'letter-col': '71.627937rem',
         'letter-col-full': '77.9375rem',
         'a4': '91.350883rem',
-        'a4-col': '77.49132rem',
+        'a4-col': '77.04132rem',
         'a4-col-full': '83.350883rem',
       },
       spacing: {
@@ -56,7 +56,7 @@ module.exports = {
         '2.5': '0.67rem',
         '4.5': '1.18rem',
         '7': '1.6rem',
-        '9': '2.3rem',
+        '11': '2.75rem',
       },
       colors: {
         gray: {
