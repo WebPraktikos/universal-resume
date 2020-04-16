e2c63a497a821c4579b9ef15c1e03ac267a04bab
WebPraktikos <hi@webpraktikos.com>
2020-04-16 22:34:03 +0200

    design: Change page height values and other

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,10 +42,10 @@ module.exports = {
       },
       height: {
         "letter": "85.9375rem",
-        "letter-col": "71.627937rem",
+        "letter-col": "71.625rem",
         "letter-col-full": "77.9375rem",
         "a4": "91.350883rem",
-        "a4-col": "77.04132rem",
+        "a4-col": "77.038383rem",
         "a4-col-full": "83.350883rem",
       },
       spacing: {
